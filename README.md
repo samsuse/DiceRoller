8c80d92eff3feabc2eb51109c30fa0f0f0a33dcd
Sam Zhao <20300075+samsuse@users.noreply.github.com>
2023-12-16 22:21:23 +0800

    Upgrade to Compose version

---
+++ README.md
@@ -0,0 +1,26 @@
+Project: DiceRoller App
+==================================
+
+Solution code for the Android Basics with Compose: Create an interactive dice roller app.
+
+Introduction
+------------
+In this codelab, you will build out the Dice Roller app from start to finish using Compose.
+
+Pre-requisites
+--------------
+
+You need to know:
+- How to create a new project in Android Studio
+- How to build a basic layout with Compose.
+- How to write control flow statements (if / else, when statements).
+
+Getting Started
+---------------
+
+1. Download and run the app.
+
+License
+---
+
+[DiceRoller](https://github.com/samsuse/DiceRoller) is under the Apache 2.0 license. See the [LICENSE](https://github.com/samsuse/DiceRoller/blob/master/LICENSE) file for details.
--- app/src/main/java/com/samsuse/app/diceroller/MainActivity.kt
+++ app/src/main/java/com/samsuse/app/diceroller/MainActivity.kt
@@ -2,72 +2,92 @@ package com.samsuse.app.diceroller
 
 import android.os.Bundle
 import android.util.Log
-import android.widget.Button
-import android.widget.ImageView
-import androidx.appcompat.app.AppCompatActivity
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.samsuse.app.diceroller.ui.theme.DiceRollerTheme
+
+
+private val TAG = "MainActivity"
 
 /**
  * This activity allows the user to roll a dice and view the result
  * on the screen.
  */
-class MainActivity : AppCompatActivity() {
-
-    private val TAG = "MainActivity"
+class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        // Find the Button in the layout
-        val rollButton: Button = findViewById<Button>(R.id.button)
-
-        rollButton.setOnClickListener { rollDice() }
-
-        // Do a dice roll when the app starts
-        rollDice()
-    }
-
-    /**
-     * Roll the dice and update the screen with the result.
-     */
-    private fun rollDice() {
-        Log.i(TAG, "INFO: roll dice")
-
-        // Create new Dice object with 6 sides and roll the dice
-        val dice = Dice(6)
-        val diceRoll = dice.roll()
-
-        // Find the ImageView in the layout
-        val diceImage: ImageView = findViewById<ImageView>(R.id.imageView)
-
-        // Determine which drawable resource ID to use based on the dice roll
-        val drawableResource = when (diceRoll) {
-            1 -> R.drawable.dice_1
-            2 -> R.drawable.dice_2
-            3 -> R.drawable.dice_3
-            4 -> R.drawable.dice_4
-            5 -> R.drawable.dice_5
-            else -> R.drawable.dice_6
+        setContent {
+            DiceRollerTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    DiceRollerApp()
+                }
+            }
         }
-
-        // Update the ImageView with the correct drawable resource ID
-        diceImage.setImageResource(drawableResource)
-
-        // Update the content description
-        diceImage.contentDescription = diceRoll.toString()
-
     }
+
 }
 
-/**
- * Dice with a fixed number of sides.
- */
-class Dice(private val numSides: Int) {
+@Preview
+@Composable
+fun DiceRollerApp() {
+    DiceWithButtonAndImage(
+        modifier = Modifier
+            .fillMaxSize()
+            .wrapContentSize(Alignment.Center)
+    )
+}
 
-    /**
-     * Do a random dice roll and return the result.
-     */
-    fun roll(): Int {
-        return (1..numSides).random()
+
+/**
+ * Roll the dice and update the screen with the result.
+ */
+@Composable
+private fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
+
+    Log.i(TAG, "INFO: roll dice")
+
+    var diceRoll by remember { mutableStateOf(1) }
+
+    // Determine which drawable resource ID to use based on the dice roll
+    val imageResource = when (diceRoll) {
+        1 -> R.drawable.dice_1
+        2 -> R.drawable.dice_2
+        3 -> R.drawable.dice_3
+        4 -> R.drawable.dice_4
+        5 -> R.drawable.dice_5
+        else -> R.drawable.dice_6
+    }
+
+    Column(modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally) {
+        Image(painter = painterResource(imageResource), contentDescription = diceRoll.toString())
+
+        Button(
+            onClick = { diceRoll = (1..6).random() },
+        ) {
+            Text(text = stringResource(R.string.roll), fontSize = 24.sp)
+        }
     }
 }
